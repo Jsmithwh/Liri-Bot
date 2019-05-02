66bd7ff4788d77b04d05e67add0fd48c1cdca54f
jsmithwh <46731513+Jsmithwh@users.noreply.github.com>
2019-05-02 18:22:00 -0500

    edited code for bands api

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.2595885d6dae41afad7e22109e1f85ee,
-  secret: process.env.de71b31c16794eaca0300852060dfe02
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
